abd4d1a1802201b733f4b4a8328b87479361dcf5
yafacex <yafacex@163.com>
2023-04-25 23:03:55 +0800

    echo log

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -177,10 +177,18 @@ cd ..
 if [ "$SHARED_LIB" = "ON" ];then
     cp build32/libTNN.so release/armeabi-v7a
     cp build64/libTNN.so release/arm64-v8a
+	echo "TNN_BUILD_PATH v7a so:$(PWD)/release/armeabi-v7a/libTNN.so"
+	echo "TNN_BUILD_PATH v8a so:$(PWD)/release/arm64-v8a/libTNN.so"
 else
     cp build32/libTNN.a release/armeabi-v7a
     cp build64/libTNN.a release/arm64-v8a
+	echo "TNN_BUILD_PATH v7a so:$(PWD)/release/armeabi-v7a/libTNN.a"
+	echo "TNN_BUILD_PATH v8a so:$(PWD)/release/arm64-v8a/libTNN.a"
 fi
+echo "show releases"
+ls $(PWD)/release/armeabi-v7a
+ls $(PWD)/release/arm64-v8a
+
 cp -r ${TNN_ROOT_PATH}/include release
 if [  "$HUAWEI_NPU" == "ON" ]; then
     cp ${TNN_ROOT_PATH}/third_party/huawei_npu/hiai_ddk_latest/armeabi-v7a/* release/armeabi-v7a/
